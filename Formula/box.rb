99533c0227cff5ebb4e78b539e85774cd346e28d
Erik Hollensbe <github@hollensbe.org>
2017-03-05 23:36:30 -0800

    box.rb: move to portable binary for v0.5.0
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,11 +1,11 @@
 class Box < Formula
   desc "A Next-Generation Builder for Docker Images"
   homepage "https://erikh.github.io/box/"
-  url "https://github.com/erikh/box/releases/download/v0.4.2/box-0.4.2.darwin.gz"
+  url "https://github.com/erikh/box/releases/download/v0.5.0/box-0.4.2.portable.gz"
   sha256 "40d2561b197a9e4460ba02f7499ee2c52a19a2bfe38d9baa3aafc4d4e883f5bc"
 
   def install
-    mv "box-0.4.2.darwin", "box"
+    mv "box-0.4.2.portable", "box"
     bin.install "box"
   end
   test do
