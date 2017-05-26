a50a9800e8af36c78e948373b46689193011acba
Erik Hollensbe <github@hollensbe.org>
2017-05-26 02:20:03 -0700

    fix portable sha256. So sorry
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- Formula/box.rb
+++ Formula/box.rb
@@ -4,7 +4,7 @@ class Box < Formula
   desc "A Next-Generation Builder for Docker Images"
   homepage "https://box-builder.github.io/box/"
   url "https://github.com/box-builder/box/releases/download/v#{@@boxVersion}/box-#{@@boxVersion}.portable.gz"
-  sha256 "db9b3d2ca2784665ba3324f5903b3ebceacb6399e11df03918404c5f56d1a152"
+  sha256 "746565291ed18fe4dabd7d6d3dabc13c133d8d2b34f79a302aa61342c00a899f"
 
   def install
     mv "box-#{@@boxVersion}.portable", "box"
