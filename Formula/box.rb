003fee07640dab17a3e97215e87693697b3eb0c2
Erik Hollensbe <github@hollensbe.org>
2017-08-16 22:56:06 -0700

    box 0.5.6
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,10 +1,10 @@
 class Box < Formula
-  @@boxVersion = "0.5.5"
+  @@boxVersion = "0.5.6"
 
   desc "A Next-Generation Builder for Docker Images"
   homepage "https://box-builder.github.io/box/"
   url "https://github.com/box-builder/box/releases/download/v#{@@boxVersion}/box-#{@@boxVersion}.portable.gz"
-  sha256 "a72152eb9f6a2aa289b08198be22c2403b690d6c7221c880a1cb5ae05a33bb77"
+  sha256 "ef8d73b3cfff2defc38bad2c6463d05bcfb92d153c02f653bcf1b91ed454f2e6"
 
   def install
     mv "box-#{@@boxVersion}.portable", "box"
